4fc47c86736c9120487b324297f9682031e00685
Happyboy171 <luckyboy1992330@mail.com>
2016-04-06 13:35:18 +0800

    Modify sql queries.

--- database/ObserveFirst.sql
+++ database/ObserveFirst.sql
@@ -40,3 +40,23 @@ CREATE TABLE `Users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'PieterFabry','poop','Pieter','Fabry',0,'pieterfabry@observe.tech','2017-02-07 10:29:57','2017-02-07 10:29:57'),(2,'admin','admin','admin','admin',0,'admin@observe.tech','2017-02-07 10:29:57','2017-02-07 10:29:57'),(3,'Farmer','Farmer','Martin','Fabry',0,'martin@observe.tech','2017-02-07 10:29:57','2017-02-07 10:29:57'),(4,'Manager','Manager','Jon','Fabry',0,'jon@observe.tech','2017-02-07 10:29:57','2017-02-07 10:29:57');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-21 11:25:13
